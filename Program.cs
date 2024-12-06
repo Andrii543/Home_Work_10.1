3e76a31a7e169b22861ae048c0f8dd99887fb5e8
Andrii543 <andrii.rusyn15@gmail.com>
2024-12-06 18:35:55 +0100

    Compare first letters with comments added

--- Program.cs
+++ Program.cs
@@ -8,6 +8,13 @@ namespace Home_Wokr_10._1
         {
             Console.OutputEncoding = UTF8Encoding.UTF8;
 
+
+            //Я зробив два різних вивода
+            //1: Це коли ми вписуєм ім'я та прізвище тобто коли ми незнаєм що користувач введе
+            //2: Це коли ми знаєм що знаходиться під зміннами ім'я та прізвища 
+
+
+            // 1 Спосіб
             Console.Write("Введіть своє ім'я: ");
             string inputFistName = Console.ReadLine();
 
@@ -28,21 +35,22 @@ namespace Home_Wokr_10._1
             }
 
 
-            //string name = "Andrii";
-            //string lastName = "Rusyn";
+            // 2 Спосіб
+            string name = "Andrew";
+            string lastName = "Rusyn";
 
 
-                //string removeName = name.Remove(1, 5);
-                //string removeLastName = lastName.Remove(1, 4);
+            string removeName = name.Remove(1, 5);
+            string removeLastName = lastName.Remove(1, 4);
 
-                //if (removeName == removeLastName)
-                //{
-                //    Console.WriteLine($"Прізвище починається на ту ж літеру, що і ім'я:\nПерша літера ім'я {removeName}\nПерша літера прізвища {removeLastName}");
-                //}
-                //else
-                //{
-                //    Console.WriteLine($"Прізвище не починається на ту ж літеру, що і ім'я:\nПерша літера ім'я {removeName}\nПерша літера прізвища {removeLastName}");
-                //}
+            if (removeName == removeLastName)
+            {
+                Console.WriteLine($"Прізвище починається на ту ж літеру, що і ім'я:\nПерша літера ім'я {removeName}\nПерша літера прізвища {removeLastName}");
+            }
+            else
+            {
+                Console.WriteLine($"Прізвище не починається на ту ж літеру, що і ім'я:\nПерша літера ім'я {removeName}\nПерша літера прізвища {removeLastName}");
+            }
 
         }
     }
